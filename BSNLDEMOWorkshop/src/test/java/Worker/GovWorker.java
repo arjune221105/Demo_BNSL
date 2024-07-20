58124cb01d19e0b082f811499c2be3e6887f0929
arjune221105 <shakti221105@gmail.com>
2024-07-19 20:30:27 -0700

    push

---
+++ BSNLDEMOWorkshop/src/test/java/Worker/GovWorker.java
@@ -0,0 +1,12 @@
+package Worker;
+
+public class GovWorker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GOV worker 10K");
+		System.out.println("Salery 50K month");
+
+	}
+
+}
